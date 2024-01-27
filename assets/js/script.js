dc4897ab8360a63ae7d3cb2157f9e629c82f19e3
bella <isakawaii365@gmail.com>
2024-01-27 20:17:33 +0000

    new code in script file declaring vars

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=London,GB&appid=746b435d2849d7f5790f34062361ed11"
-console.log(queryURL);
+
+// Get user input from the input field
+var chosenCity = $("search-input").val();
+
+// API URL
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast";
+
+// My API key 
+var APIKey = "746b435d2849d7f5790f34062361ed11";
+
+// Construct the URL with user input and API key
+
+var queryURLWithParam = queryURL + "?q=" + chosenCity + "&limit=1&appid=" + APIKey;
+console.log(queryURLWithParam);
 
